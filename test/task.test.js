45781685d1007f2413592db6c9b0650a29e77aa9
Gilles Pelegrin <gilles@wheelhouse.be>
2022-08-31 13:45:46 +0200

    adapt circle ci config

--- test/task.test.js
+++ test/task.test.js
@@ -9,8 +9,7 @@ describe('Tasks', () => {
     beforeAll(async () => {
         let server = await createServer();
         app = await server.listen(3001,)
-        console.log(pool)
-        // await pool.query('DELETE FROM task')
+        await pool.query('DELETE FROM task')
     })
 
     afterAll(async () => {
