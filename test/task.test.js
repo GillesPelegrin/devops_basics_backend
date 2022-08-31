1a5a91c6a8b787dad5faf021a727021d1b8908b5
Gilles Pelegrin <gilles@wheelhouse.be>
2022-08-31 13:23:23 +0200

    adapt circle ci config

--- test/task.test.js
+++ test/task.test.js
@@ -7,58 +7,54 @@ describe('Create Get and Delete Task', () => {
     let app;
 
     beforeAll(async () => {
-      //  let server = await createServer();
-       // app = await server.listen(3001, )
-       // await pool.query('DELETE FROM task')
-
-        console.log(process.env.DB_HOST)
-        console.log(process.env.DB_USER)
-        console.log(process.env.DB_PASSWORD)
+        let server = await createServer();
+        app = await server.listen(3001,)
+        //await pool.query('DELETE FROM task')
     })
 
     afterAll(async () => {
-      //  await pool.end().then(() => app.close())
+        await pool.end().then(() => app.close())
     })
 
 
     it('responds with json', async () => {
-       /* await request(app)
-            .post('/tasks')
-            .send({title: 'testTitle', message: 'testMessage'})
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200);
+        /* await request(app)
+             .post('/tasks')
+             .send({title: 'testTitle', message: 'testMessage'})
+             .set('Accept', 'application/json')
+             .expect('Content-Type', /json/)
+             .expect(200);
 
-        const tasks = await request(app)
-            .get('/tasks')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200);
+         const tasks = await request(app)
+             .get('/tasks')
+             .set('Accept', 'application/json')
+             .expect('Content-Type', /json/)
+             .expect(200);
 
-        expect(tasks.body.length).toBe(1)
+         expect(tasks.body.length).toBe(1)
 
-        const actualTask = tasks.body[0]
-        const taskId = actualTask.taskid;
-        delete actualTask.taskid
-        expect(actualTask).toStrictEqual(
-            {
-                message: "testMessage",
-                title: "testTitle",
-            }
-        )
+         const actualTask = tasks.body[0]
+         const taskId = actualTask.taskid;
+         delete actualTask.taskid
+         expect(actualTask).toStrictEqual(
+             {
+                 message: "testMessage",
+                 title: "testTitle",
+             }
+         )
 
-        await request(app)
-            .delete(`/tasks/${taskId}`)
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200);
+         await request(app)
+             .delete(`/tasks/${taskId}`)
+             .set('Accept', 'application/json')
+             .expect('Content-Type', /json/)
+             .expect(200);
 
-        const deletedTasks = await request(app)
-            .get('/tasks')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200);
+         const deletedTasks = await request(app)
+             .get('/tasks')
+             .set('Accept', 'application/json')
+             .expect('Content-Type', /json/)
+             .expect(200);
 
-        expect(deletedTasks.body.length).toBe(0) */
+         expect(deletedTasks.body.length).toBe(0) */
     });
 });
